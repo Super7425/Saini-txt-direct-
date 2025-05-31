5e86635e56bc71c37667fd57e71af8827217a73c
Super7425 <devisingh74255@gmail.com>
2025-05-31 19:58:07 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -3,11 +3,11 @@
 import os
 from os import environ
 
-API_ID = int(environ.get("API_ID", "xxxxxxxxx"))
-API_HASH = environ.get("API_HASH", "xxxxxxxxxxxxxxxxxxxxxxx")
+API_ID = int(environ.get("API_ID", "13005663"))
+API_HASH = environ.get("API_HASH", "74679e2189bf96d0037aa65f121e1fbf")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
-OWNER = int(environ.get("OWNER", "5680454765"))
-CREDIT = "𝄟⃝‌🐬🇳‌ɪᴋʜɪʟ𝄟⃝🐬"
+OWNER = int(environ.get("OWNER", "6696400163"))
+CREDIT = "SAINI BOTS"
 AUTH_USER = os.environ.get('AUTH_USERS', '5680454765').split(',')
 AUTH_USERS = [int(user_id) for user_id in AUTH_USER]
 if int(OWNER) not in AUTH_USERS:
